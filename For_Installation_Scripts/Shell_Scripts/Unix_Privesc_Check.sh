ac284d8a6408a8e16f6b6354a1d7ac21a3b3604b
Eudita <adiyadav1592@gmail.com>
2024-04-08 20:09:17 +0530

    Updated

--- For_Installation_Scripts/Shell_Scripts/Unix_Privesc_Check.sh
+++ For_Installation_Scripts/Shell_Scripts/Unix_Privesc_Check.sh
@@ -3,14 +3,4 @@
 # Clone Unix-privesc-check repository
 git clone https://github.com/pentestmonkey/unix-privesc-check.git
 
-# Change directory to the Unix-privesc-check directory
-cd unix-privesc-check
 
-# Run the Unix-privesc-check script
-./unix-privesc-check.sh
-
-
-chmod +x install_unix-privesc-check.sh
-
-
-./install_unix-privesc-check.sh
