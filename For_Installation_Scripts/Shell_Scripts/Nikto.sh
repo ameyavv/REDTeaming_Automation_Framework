223a21cfe918886b9bdacc683f116855824a64c8
Ameya Vaidya <103118830+ameyavv@users.noreply.github.com>
2024-04-10 00:06:22 +0530

    Delete For_Installation_Scripts directory

--- For_Installation_Scripts/Shell_Scripts/Nikto.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Clone Nikto repository
-git clone https://github.com/sullo/nikto
-
-
--- For_Installation_Scripts/Shell_Scripts/Responder.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/lgandx/Responder.git
-cd Responder
-pip install -r requirements.txt
-sudo ./Responder.py -I eth0 –Pv
-chmod +x run_responder.sh
-./run_responder.sh
