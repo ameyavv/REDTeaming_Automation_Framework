bb626e09d865f1733a395dc9bf07c4bb7a3326a8
Ameya Vaidya <103118830+ameyavv@users.noreply.github.com>
2024-04-13 23:01:25 +0530

    Update theHarvester.sh

--- Shell_Scripts/theHarvester.sh
+++ Shell_Scripts/theHarvester.sh
@@ -4,11 +4,11 @@
 git clone https://github.com/laramies/theHarvester.git
 
 # Change directory to theHarvester
-cd theHarvester
+#cd theHarvester
 
 # Run theHarvester (assuming Python is installed)
-python theHarvester.py -d example.com -b all
+#python theHarvester.py -d example.com -b all
 
-chmod +x run_theHarvester.sh
+#chmod +x run_theHarvester.sh
 
 #./run_theHarvester.sh
